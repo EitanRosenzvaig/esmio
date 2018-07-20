c441549ae2e93f5aa750f69e10676accf07702a6
Eitan <eitanrosenzvaig@gmail.com>
2018-07-20 13:47:50 -0300

    Testing desrializer

--- saleor/core/utils/json_serializer.py
+++ saleor/core/utils/json_serializer.py
@@ -1,5 +1,6 @@
 from django.core.serializers.json import (
-    DjangoJSONEncoder, Serializer as JsonSerializer)
+    DjangoJSONEncoder, Serializer as JsonSerializer,
+    Deserializer as JsonDeserializer)
 from prices import Money
 
 
@@ -9,7 +10,20 @@ class Serializer(JsonSerializer):
         self.json_kwargs['cls'] = CustomJsonEncoder
 
 
+class Deserializer(JsonDeserializer):
+    def _init_options(self):
+        super()._init_options()
+        self.json_kwargs['cls'] = CustomJsonDecoder
+
+
 class CustomJsonEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Money):
+            return "{} {}".format(obj.amount, obj.currency)
+        return super().default(obj)
+
+
+class CustomJsonDecoder(DjangoJSONEncoder):
     def default(self, obj):
         if isinstance(obj, Money):
             return "{} {}".format(obj.amount, obj.currency)
