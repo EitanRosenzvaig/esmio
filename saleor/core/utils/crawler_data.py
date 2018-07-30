437c9f32629bd2c59222c33bf444c102a06a7742
Eitan <eitanrosenzvaig@gmail.com>
2018-07-30 09:36:58 -0300

    Product creation involves upload of image to Spaces in DO

--- saleor/core/utils/crawler_data.py
+++ saleor/core/utils/crawler_data.py
@@ -3,28 +3,24 @@ import urllib.request as req
 import urllib.parse as urlparse
 from socket import timeout
 
+import boto3
+from botocore.exceptions import ClientError
+
 from django.conf import settings
-from django.contrib.auth.models import Group, Permission
 from django.contrib.sites.models import Site
 from django.core.files import File
-from faker.providers import BaseProvider
 from django.template.defaultfilters import slugify
-from django_countries.fields import Country
 from prices import Money
 
 from ...core.utils.mongo_reader import MongoReader
 from ...core.utils.mapper import get_category
 from ...core.utils.text import strip_html_and_truncate
-from ...menu.models import Menu
-from ...page.models import Page
 from ...product.models import (
     AttributeChoiceValue, Category, Product, ProductAttribute,
     ProductImage, ProductType, ProductVariant)
 from ...product.thumbnails import create_product_thumbnails
 from ...product.utils.attributes import get_name_from_attributes
-from ...shipping.models import ANY_COUNTRY, ShippingMethod
 
-from re import sub
 from decimal import Decimal
 
 # For debugg mode
@@ -48,6 +44,24 @@ BRAND_REORDER = {
                'heyas': [2,0,1,3,4]
                 }
 
+STORAGE_SESSION = boto3.session.Session()
+STORAGE_CLIENT = STORAGE_SESSION.client('s3',
+                    region_name='nyc3',
+                    endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+                    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
+)
+STORAGE_BUCKET = settings.AWS_MEDIA_BUCKET_NAME
+
+def _exists_in_s3(client, bucket, key):
+    """return the key's size if it exist, else None"""
+    try:
+        obj = client.head_object(Bucket=bucket, Key=key)
+        return obj['ContentLength']
+    except ClientError as exc:
+        if exc.response['Error']['Code'] != '404':
+            raise
+
 def sort_by_site_generic_order(urls, brand):
     if brand in BRAND_REORDER:
         order = BRAND_REORDER[brand]
@@ -63,32 +77,33 @@ def save_for_similarity(image_directory, url):
     similarity_directory = os.path.join(image_directory, 'similarity')
     if not os.path.exists(similarity_directory):
         os.makedirs(similarity_directory)
-    image = generate_image(similarity_directory, url)
+    image = generate_image(similarity_directory, url, local=False)
 
 
 def full_mongo_import(placeholder_dir):
     mongo = MongoReader()
     brands = mongo.get_all_brands()
     shuffle(brands)
+    brands = ['febo']
     for brand in brands:
         image_directory = os.path.join(placeholder_dir, brand)
         for item in mongo.get_all_products_from_brand(brand):
-            try:
-                if item['sizes'] and len(item['image_urls']) > 0:
-                    product, product_type = create_product(item)
-                    for size in item['sizes']:
-                        create_size_variant(product, size)
-                    if type(item['image_urls']) is list:
-                        urls = item['image_urls']
-                        urls = sort_by_site_generic_order(urls, brand)
-                    else:
-                        urls = [item['image_urls']]
-                    save_for_similarity(image_directory, urls[0])
-                    for product_image_url in urls:
-                        create_product_image(product, image_directory, product_image_url)
-                yield 'Product Added'
-            except Exception as e:
-                yield 'Error adding product: ' + str(e)
+            # try:
+            if item['sizes'] and len(item['image_urls']) > 0:
+                product, product_type = create_product(item)
+                for size in item['sizes']:
+                    create_size_variant(product, size)
+                if type(item['image_urls']) is list:
+                    urls = item['image_urls']
+                    urls = sort_by_site_generic_order(urls, brand)
+                else:
+                    urls = [item['image_urls']]
+                save_for_similarity(image_directory, urls[0])
+                for product_image_url in urls:
+                    create_product_image(product, image_directory, product_image_url)
+            yield 'Product Added'
+            # except Exception as e:
+            #     yield 'Error adding product: ' + str(e)
 
 
 def get_product_type(name):
@@ -163,27 +178,36 @@ def create_product_image(product, placeholder_dir, url):
     return product_image
 
 
-def generate_image(image_dir, url):
-    image_name = str(hash(url)) + '.jpg'
+def generate_image(image_dir, url, local=False):
+    # Fix wrongly encoded URL strings:
+    if not url.startswith('http'):
+        url = 'https://' + url
+    url = urlparse.urlsplit(url)
+    url = list(url)
+    url[2] = urlparse.quote(url[2])
+    url = urlparse.urlunsplit(url)
+    request = req.Request(url, headers=HEADERS)
+    image_name = str(hash(url)) + '.jpg' # TODO: Check if PNG
     file_path = os.path.join(image_dir, image_name)
-    if not os.path.isfile(file_path):
-        f = open(file_path, 'wb')
-        # Fix wrongly encoded URL strings:
-        if not url.startswith('http'):
-            url = 'https://' + url
-        url = urlparse.urlsplit(url)
-        url = list(url)
-        url[2] = urlparse.quote(url[2])
-        url = urlparse.urlunsplit(url)
+    if not local:
+        exists = _exists_in_s3(
+            STORAGE_CLIENT, STORAGE_BUCKET, file_path) is not None
+    else:
+        exists = os.path.isfile(file_path)
+    if not exists:
         try:
-            request = req.Request(url, headers=HEADERS)
-            f.write(req.urlopen(request, timeout=10).read())
+            image = req.urlopen(request, timeout=10).read()
+            if not local:
+                STORAGE_CLIENT.put_object(Key=file_path, Body=image,
+                            Bucket=STORAGE_BUCKET)
+            else:
+                f = open(file_path, 'wb')
+                f.write(image)
+                f.close()
         except timeout:
             print('socket timed out - URL %s', url)
             file_path = NA_IMAGE_PATH
         except:
             print('get image error')
             file_path = NA_IMAGE_PATH
-        f.close()
-
-    return File(open(file_path, 'rb'))
+    return file_path
