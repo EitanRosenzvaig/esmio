62f50fb2a43418efc3ba7f95acf1c98e23376f15
Eitan <eitanrosenzvaig@gmail.com>
2018-08-17 16:34:17 -0300

    Search hot fix

--- saleor/search/backends/postgresql_storefront.py
+++ saleor/search/backends/postgresql_storefront.py
@@ -21,5 +21,5 @@ def search(phrase):
     ft_in_description = Q(description__search=phrase)
     name_similar = Q(name_sim__gt=0.2)
     brand_similar = Q(brand_sim__gt=0.2)
-    return Product.objects.annotate(name_sim=name_sim).filter(
+    return Product.objects.annotate(name_sim=name_sim, brand_sim=brand_sim).filter(
         (ft_in_description | name_similar | brand_similar) & published)
